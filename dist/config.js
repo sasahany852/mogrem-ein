abd2249492908731c3f571be39d1a9c933dae13b
sasahany852 <87889684+sasahany852@users.noreply.github.com>
2025-10-04 05:39:08 +0300

    Update config.js

--- dist/config.js
+++ dist/config.js
@@ -7,7 +7,7 @@ const config = {
     clientId: '1391597625211621427', // bot id
     mongoUri: 'mongodb://sasahany88:42227914012@db.wick.ink:27017/db_sasahany88?authSource=admin', // mongodb url
     defaultPrefix: '/',
-    mainGuildId: '1130874774227255369', // main guild id
+    mainGuildId: '1171716640270987375', // main guild id
     defaultLanguage: 'en',
     dashboard: {
         port: 3000,     // port for dashboard
